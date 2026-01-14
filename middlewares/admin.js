7c1c9264f50d422177e643077c0f2788126c1628
Vivek Singh <notanetizen@Viveks-MacBook-Air-4.local>
2026-01-14 22:32:48 +0530

    updated admin middleware

--- middlewares/admin.js
+++ middlewares/admin.js
@@ -3,7 +3,7 @@ const { AdminModel } = require("../db");
 
 const adminMiddlware = ( req, res, next) => {
 
-    const token = req.headers.authorization;
+    const token = req.headers.authorization.split(" ")[1];
     const decoded = jwt.verify(token, process.env.JWT_ADMIN_SECRET);
 
     if(decoded) {
